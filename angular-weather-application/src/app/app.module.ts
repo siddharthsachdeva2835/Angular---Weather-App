e050451d16fc39198dcd7ad83b8b68b7c53d392b
siddharthsachdeva2835 <siddharth.sachdeva@nagarro.com>
2018-10-08 18:28:07 +0530

    navbar and routing added

--- angular-weather-application/src/app/app.module.ts
+++ angular-weather-application/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WeatherComponent } from './weather/weather.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { RouterModule , Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: 'current-weather', component: WeatherComponent},
+  {path: '5-day-forecast', component: HeaderComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WeatherComponent,
+    ForecastComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
