9986d96c49d63d1f3b7a18c068f20779d70388f4
siddharthsachdeva2835 <siddharthsachdeva2835@gmail.com>
2018-10-09 14:48:46 +0530

    final submission

--- angular-weather-application/src/app/forecast/forecast.component.ts
+++ angular-weather-application/src/app/forecast/forecast.component.ts
@@ -27,7 +27,8 @@ export class ForecastComponent implements OnInit {
       this.weatherForecasts = [];
       for (let forecast of data.list) {
         if ( i % 8 === 0 ) {
-          this.weatherForecasts.push(new ForecastModel(forecast.dt_txt, forecast.main.temp + '9\xB0' + 'F',
+          const date = forecast.dt_txt.slice(0, 10);
+          this.weatherForecasts.push(new ForecastModel(date, forecast.main.temp + '9\xB0' + 'F',
           'http://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png', forecast.weather[0].description,
                           'Min: ' + forecast.main.temp_min + '9\xB0' + 'F', 'Max: '
                             + forecast.main.temp_max + '9\xB0' + 'F'));
